f69937451522f8726b71ebbf535be4fbf9af65ac
Soham <sohamborkar14@gmail.com>
2023-01-15 23:23:06 +0530

    dikstras algo added

---
+++ C language/DSA/dijkstra_programiz.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#define INFINITY 99
+#define MAX 10
+
+void getgraph(int n, int Graph[MAX][MAX])
+{
+    printf("Enter graph: \n");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            scanf("%d", &Graph[i][j]);
+        }
+    }
+}
+
+void Dijkstra(int Graph[MAX][MAX], int cost[MAX][MAX], int n, int start)
+{
+    int distance[MAX], pred[MAX];
+    int visited[MAX], count, mindistance, nextnode, i, j;
+
+    // Creating cost matrix
+    for (i = 0; i < n; i++)
+        for (j = 0; j < n; j++)
+            if (Graph[i][j] == 0)
+                cost[i][j] = INFINITY;
+            else
+                cost[i][j] = Graph[i][j];
+
+    for (i = 0; i < n; i++)
+    {
+        distance[i] = cost[start][i];
+        pred[i] = start;
+        visited[i] = 0;
+    }
+
+    distance[start] = 0;
+    visited[start] = 1;
+    count = 1;
+
+    while (count < n - 1)
+    {
+        mindistance = INFINITY;
+
+        for (i = 0; i < n; i++)
+            if (distance[i] < mindistance && !visited[i])
+            {
+                mindistance = distance[i];
+                nextnode = i;
+            }
+
+        visited[nextnode] = 1;
+        for (i = 0; i < n; i++)
+            if (!visited[i])
+                if (mindistance + cost[nextnode][i] < distance[i])
+                {
+                    distance[i] = mindistance + cost[nextnode][i];
+                    pred[i] = nextnode;
+                }
+        count++;
+    }
+
+    // Printing the distance
+    for (i = 0; i < n; i++)
+        if (i != start)
+        {
+            printf("\nDistance from source to %d: %d \n", i, distance[i]);
+            printf("Path =  %d ", i);
+            j = i;
+            do
+            {
+                j = pred[j];
+                printf("From %d ", j);
+            } while (j != start);
+            printf("\n");
+        }
+}
+
+int main()
+{
+    int Graph[MAX][MAX], i, j, n = 7, u, cost[MAX][MAX];
+    printf("Enter the number of vertices: ");
+    scanf("%d", &n);
+
+    getgraph(n, Graph);
+/*
+0 0 1 2 0 0 0
+0 0 2 0 0 3 0
+1 2 0 1 3 0 0
+2 0 1 0 0 0 1
+0 0 3 0 0 2 0
+0 3 0 0 2 0 1
+0 0 0 1 0 1 0
+*/
+    u = 0;
+    Dijkstra(Graph, cost, n, u);
+
+    return 0;
+}
---
+++ C language/DSA/dijkstras.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#define INFINITY 99
+#define MAX 10
+#define startnode 0
+//  #define is preprocessor directory, which replace value of max as 10 during compilation starts
+
+void getgraph(int cost[MAX][MAX], int n, int u)
+{
+    printf("Enter the number of Vertices: \n");
+    scanf("%d", &n);
+    printf("Enter cost matrix: \n");
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            scanf("%d", &cost[i][j]);
+};
+
+void dijkstra(int cost[MAX][MAX], int n)
+{
+    int distance[MAX], predefined[MAX], visited[MAX];
+    int count, mindistance, nextnode, j;
+
+    for (int i = 0; i < n; i++)
+    {
+        distance[i] = cost[startnode][i];
+        predefined[i] = startnode; // it is 0 decleared
+        visited[i] = 0;
+    }
+    distance[startnode] = 0;
+    visited[startnode] = 1;
+    count = 1;
+
+    while (count < n - 1)
+    {
+        mindistance = INFINITY;
+        for (int i = 0; i < n; i++)
+            if (distance[i] < mindistance && !visited[i])
+            {
+                mindistance = distance[i];
+                nextnode = i;
+            }
+
+        // check if better path exists through nextnode
+        visited[nextnode] = 1;
+        for (int i = 0; i < n; i++)
+            if (!visited[i])
+                if (mindistance + cost[nextnode][i] < distance[i])
+                {
+                    distance[i] = mindistance + cost[nextnode][i];
+                    predefined[i] = nextnode;
+                }
+        count++;
+    }
+
+    // printing the path and distance of each node
+    for (int i = 0; i < n; i++)
+    {
+        if (i != startnode)
+        {
+            printf("Distance of node %d = %d \n", i, distance[i]);
+            printf("Path =  %d  ", i);
+            j = i;
+            do
+            {
+                j = predefined[j];
+                printf("<- %d \n", j);
+            } while (j != startnode);
+        }
+    }
+};
+
+int main()
+{
+    int cost[MAX][MAX], n, u;
+    getgraph(cost, n, u);
+
+    dijkstra(cost, n);
+
+    return 0;
+}
